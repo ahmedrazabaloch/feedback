cfc99ef408cb80ff8e43dd55f8658d9fd94fabb2
ahmedraza <ahmed.raza1054@gmail.com>
2024-08-04 04:06:09 +0500

    added super admin

--- backend/server/scripts/createAdmin.js
+++ backend/server/scripts/createAdmin.js
@@ -37,7 +37,6 @@
 // };
 
 // createOrUpdateAdmin();
-
 const mongoose = require("mongoose");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
@@ -45,12 +44,17 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const createOrUpdateAdmin = async () => {
   try {
-    const email = "ahmedraza@gmail.com";
-    const password = "pakistan";
+    const name = "admin";
+    const rollNo = "admin-roll-no";
+    const email = "admin@gmail.com";
+    const password = "adminadmin";
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -62,6 +66,8 @@ const createOrUpdateAdmin = async () => {
       console.log("Admin user updated:", existingUser);
     } else {
       const newAdmin = await User.create({
+        name,
+        rollNo,
         email,
         password: hashedPassword,
         isAdmin: true,
