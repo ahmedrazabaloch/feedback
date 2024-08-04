9450f958a9a311794fd7812b6506d08528fddbd8
ahmedraza <ahmed.raza1054@gmail.com>
2024-08-05 01:15:09 +0500

    removed useless code

--- backend/server/routes/adminRoutes.js
+++ backend/server/routes/adminRoutes.js
@@ -1,16 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const { protect, admin } = require("../middleware/authMiddleware"); // Adjust path if needed
-// const { createAdmin, getAdmins } = require("../controllers/adminController");
-
-// // Route to create an admin (for initial setup, maybe)
-// router.post("/create", createAdmin);
-
-// // Route to get all admins (only accessible by an admin)
-// router.get("/", protect, admin, getAdmins);
-
-// module.exports = router;
-
 const express = require('express');
 const router = express.Router();
 const { protect, admin } = require('../middleware/authMiddleware');
